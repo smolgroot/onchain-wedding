27c3acf208d26134dffb84e7a799d211858bac60
smolgroot <clement.canuto@gmail.com>
2023-07-22 15:37:13 +0200

    [SMART-CONTRACT] add some marriage logic

--- cli/contract/Wedding.sol
+++ cli/contract/Wedding.sol
@@ -6,33 +6,75 @@ pragma solidity ^0.8.0;
 contract Wedding {
 
 
-    // // WEDDING LOGIC
-    // // ------------------------------------------------------
+    // WEDDING LOGIC
+    // ------------------------------------------------------
 
     struct Fiance  {
-        address sender;
+        address walletAddr;
+        string firstname;
         string description;
-        uint timestamp;
+        string ipfsPhotoCid;
+        bool alreadySaidYes;
     }
 
-    // Manage those vars in a constructor
-    Fiance fiance1;
-    Fiance fiance2;
+    // Hardcoded fiances addresses here
+    Fiance fiance1 = Fiance(
+        0xE42749aA40040929cd8db5ac5200df12f9B52540, 
+        "Rajeev", 
+        "Hello there, i'm about the get married", 
+        "QmSQTz4CxHmrNapTjYZcYdT46qJRjJN29XFj2PtrvFp6Lv",
+        false
+    );
+
+    Fiance fiance2 = Fiance(
+        0x23bC9eB2343125b91a7d5c59038e40E0AE451551, 
+        "Maria", 
+        "This is the most exciting day in my life!", 
+        "QmX4aCHjcuqpDysJk6WDLj69sYTtMGhyTypLu2uwLGKNYg",
+        false
+    );
     
     Fiance[] signers;
 
     event NewSignature(address indexed from, uint timestamp);
+    event NotAllowed(address indexed from, uint timestamp);
+    event MarriageDone(uint timestamp);
 
     function sayYes() public {
-        // @TODO --> check if addy is one from fiance1/2 
-        signers.push(fiance1);
-        emit NewSignature(msg.sender, block.timestamp);
+
+        // Check if sender is one of the 2 fiances
+        if (msg.sender == fiance1.walletAddr) {
+            fiance1.alreadySaidYes = true;
+            signers.push(fiance1);
+            emit NewSignature(fiance1.walletAddr, block.timestamp);
+        } else if (msg.sender == fiance2.walletAddr)  {
+            fiance2.alreadySaidYes = true;
+            signers.push(fiance2);
+            emit NewSignature(fiance2.walletAddr, block.timestamp);
+        } else {
+            // if not one of the 2 fiances, emit an NotAllowed event
+            emit NotAllowed(msg.sender, block.timestamp);
+        }
+
+        // Check if both peers accept the wedding contract
+        // If both said "Yes", then emit an event
+        if (getSignersCount() == 2) {
+            emit MarriageDone(block.timestamp);
+        }   
+
+    }
+
+    function getSignersCount() public view returns(uint count) {
+        return signers.length;
     }
 
     function updateFianceDescription(Fiance memory fiance) public {
         // TODO
     }
 
+    function updateFiancePhoto(Fiance memory fiance) public {
+        // TODO
+    }
 
     // DIVORCE LOGIC
     // ------------------------------------------------------
